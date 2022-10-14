35c0cfd5b4ed2a836bfc9378663ca3b6fa95cf55
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-10-13 21:18:52 -0300

    commit

--- cap1/react-dom-estrutura/index.js
+++ cap1/react-dom-estrutura/index.js
@@ -1,1 +1,34 @@
-const header = React.createElement("header", {}, "Editora MaujorTec");
+function Header() {
+    return React.createElement("header", {className: "topo"},
+    React.createElement("h1", {className: "logo"}, "Editora MaujorTec"));
+}
+
+function Navegacao() {
+    return React.createElement("div", {className: "navegacao"}, "Coluna de Navegação");
+}
+
+function Principal() {
+    return React.createElement("div", {className: "principal"}, "Coluna Principal");
+}
+
+function Rodape() {
+    return React.createElement("footer", {className: "rodape"},
+    React.createElement("p", {}, "Rodapé"));
+}
+
+function Topo() {
+    return React.createElement("div", {className: "c-topo"},
+    Header(),
+    Navegacao()
+    );
+};
+
+function App() {
+    return React.createElement(React.Fragment, {},
+        Topo(),
+        Principal(),
+        Rodape()
+        );
+};
+
+ReactDOM.render(App(), document.getElementById("root"));
