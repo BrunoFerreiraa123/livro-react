a939580bf540c8a899437cf793ed21478862607e
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-12-06 21:10:41 -0300

    commit

--- cap3/tabela-livros/src/App.js
+++ cap3/tabela-livros/src/App.js
@@ -1,15 +1,34 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import TabelaHead from "./components/TabelaHead";
 import TabelaBody from "./components/TabelaBody";
 import TabelaFoot from "./components/TabelaFoot";
 
 class App extends Component {
+  state = {
+    livros: [
+      {
+        id: "978-85-7522-632-2",
+        titulo: "CSS Grid Layout",
+        autor: "Mauricio Samy Silva"
+      },
+      {
+        id: "978-85-7522-677-3",
+        titulo: "Node Essencial",
+        autor: "<auricio Samy Silva"
+      },
+      {
+        id: "978-85-7522-512-7",
+        titulo: "Material Deesign",
+        autor: "Mauricio Samy Silva"
+      }
+    ]
+  };
   render() {
     return (
       <table className="tabela">
         <TabelaHead />
-        <TabelaBody />
         <TabelaFoot />
+        <TabelaBody livros = {this.state.livros}/>
       </table>
     );
   }
